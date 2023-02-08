4d74db6e03d0a228050e061173f0425214578aef
skipmin <83446768+skipmin@users.noreply.github.com>
2023-02-08 08:41:35 +0200

    Addition unit tests collection

--- Collection.UnitTests/CollectionTests.cs
+++ Collection.UnitTests/CollectionTests.cs
@@ -56,21 +56,103 @@ namespace Collection.UnitTests
         }
 
         [Test]
+        public void Test_Collection_AddWithGrow()
+        {
+            var coll = new Collections<int>();
+            int oldCapacity = coll.Capacity;
+            var newColl = Enumerable.Range(1000, 2000).ToArray();
+
+            coll.AddRange(newColl);
+
+            string expectedNums = "[" + string.Join(", ", newColl) + "]";
+            Assert.That(coll.ToString(), Is.EqualTo(expectedNums));
+            Assert.That(coll.Capacity, Is.GreaterThanOrEqualTo(oldCapacity));
+            Assert.That(coll.Capacity, Is.GreaterThanOrEqualTo(coll.Count));
+        }
+
+        [Test]
+        public void Test_Collection_AddRange()
+        {
+            var coll = new Collections<int>(6, 9);
+            var newColl = Enumerable.Range(10, 50).ToArray();
+
+            coll.AddRange(newColl);
+
+            Assert.That(coll.Capacity, Is.GreaterThanOrEqualTo(coll.Count));
+        }
+
+        [Test]
         public void Test_Collection_GetByIndex()
         {
-            var coll = new Collections<int>(3, 6, 9);
+            // Dimo's get by index
+            /*var coll = new Collections<int>(3, 6, 9); 
             var item = coll[1];
 
-            Assert.That(item.ToString, Is.EqualTo("6"));
+            Assert.That(item.ToString, Is.EqualTo("6"));*/
+
+            // Softuni's get by index
+            // Arrange
+            var names = new Collections<string>("Peter", "Maria");
+
+            // Act
+            var item0 = names[0];
+            var item1 = names[1];
+
+            //Assert
+            Assert.That(item0, Is.EqualTo("Peter"));
+            Assert.That(item1, Is.EqualTo("Maria"));
         }
 
         [Test]
         public void Test_Collection_SetByIndex()
         {
-            var coll = new Collections<int>(3, 6, 9);
-            var item = coll[1];
+            var coll = new Collections<int>(3, 666, 9);
+            coll[1] = 666;
 
-            Assert.That(item.ToString, Is.EqualTo("6"));
+            Assert.That(coll.ToString(), Is.EqualTo("[3, 666, 9]"));
+        }
+
+        [Test]
+        public void Test_Collection_SetByInvalidIndex()
+        {
+            var coll = new Collections<int>(17, 666, 23);
+            coll[1] = 666;
+
+            Assert.That(() => { var coll = coll[1]; },
+                Throws.InstanceOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void Test_Collection_GetByInvalidIndex()
+        {
+            // Dimo's get by invalid index
+            /*var coll = new Collections<string>("Alex", "Borr");
+
+            Assert.That(() => { var item = coll[2]; },
+                Throws.InstanceOf<ArgumentOutOfRangeException>());*/
+
+            // Softuni's get by invalid index
+            var names = new Collections<string>("Bob", "Joe");
+            Assert.That(() => { var name = names[-1]; },
+                Throws.InstanceOf<ArgumentOutOfRangeException>());
+            Assert.That(() => { var name = names[2]; },
+                Throws.InstanceOf<ArgumentOutOfRangeException>());
+            Assert.That(() => { var name = names[500]; },
+                Throws.InstanceOf<ArgumentOutOfRangeException>());
+            Assert.That(names.ToString(), Is.EqualTo("[Bob, Joe]"));
+        }
+
+        [Test]
+        public void Test_Collection_ToStringNestedCollections()
+        {
+            var names = new Collections<string>("Teddy", "Gerry");
+            var nums = new Collections<int>(10, 20);
+            var dates = new Collections<DateTime>(); 
+
+            var nested = new Collections<object>(names, nums, dates);
+            string nestedToString = nested.ToString();
+
+            Assert.That(nestedToString, Is.EqualTo("[[Teddy, Gerry], [10, 20], []]"));
         }
     }
 }
